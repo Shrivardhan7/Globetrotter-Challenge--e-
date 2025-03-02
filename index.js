970bde5ffb5eff9c2a75a1f55baee5556f9af0d4
Shrivardhan <95779170+Shrivardhan7@users.noreply.github.com>
2025-03-02 08:09:06 +0530

    src

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-// index.js
-function e(input) {
-    // Your logic goes here
-    let result = input;  // Modify this based on the problem requirements
-    return result;
-}
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-// Export the function if you're testing it with a testing framework like Jest
-module.exports = e;
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();

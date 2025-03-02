1deb0b38374c4d28ed8e2a65c227ba016031e299
shrivardhan7 <shrivardhanjadhav9@gmail.com>
2025-03-02 07:35:32 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// index.js
+function e(input) {
+    // Your logic goes here
+    let result = input;  // Modify this based on the problem requirements
+    return result;
+}
+
+// Export the function if you're testing it with a testing framework like Jest
+module.exports = e;
